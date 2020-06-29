732e6c8c9a20716a496273bc1d05010e7e942d84
root <root@localhost.localdomain>
2020-06-29 02:20:48 -0400

    docker compose

--- setting/config.py
+++ setting/config.py
@@ -1,13 +1,11 @@
 
-ROOT_DIR = "/root/airflow_perosnal/airflow_pdf_etl"
+ROOT_DIR = "/usr/local/airflow/dags"
 PDF_DIR = ROOT_DIR + "/data"
 # PDF_DIR = "/root/test"
 INGESTED_DIR = ROOT_DIR + "/ingested_data"
 ERROR_ON_INGESTION_DIR = ROOT_DIR + "/error_on_ingestion"
-
-
 ELASTICSEARCH = {
-    "URL": "http://localhost:9200",
+    "URL": "http://elasticsearch:9200",
     "HEADERS": {
         "Content-type": "application/json"
     },
@@ -16,7 +14,7 @@ ELASTICSEARCH = {
     "TIMEOUT": 10
 }
 TIKA_SERVER = {
-    "URL":"http://localhost:9998",
+    "URL":"http://tikaserver:9998",
 
     "CONTENT_HEADERS": {
         "Content-type": "application/pdf"
