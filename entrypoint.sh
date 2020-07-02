a4a87e08469082a16a25cc908b55f843adb50a11
root <root@localhost.localdomain>
2020-07-02 12:07:45 -0400

    airflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,8 +33,10 @@ done
 
 exec 6>&-
 exec 6<&-
-#airflow initdb
-#airflow webserver -p 8080 &
-#airflow scheduler
-/usr/local/bin/python /usr/local/airflow/dags/main.py 
+echo "hello"
+whoami
+airflow initdb
+airflow webserver  &
+airflow scheduler
+#/usr/local/bin/python /usr/local/airflow/dags/main.py 
 tail -f /dev/null &
