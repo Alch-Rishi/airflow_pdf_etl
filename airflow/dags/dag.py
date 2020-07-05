f0fa7e405d92f7b9d96cfedbb2ad9a4a3c3a92ac
Luvlesh Garg <luvleshgarg@ip-10-100-10-68.ap-south-1.compute.internal>
2020-07-05 20:45:00 +0530

    process type

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -16,5 +16,5 @@ dag = DAG('pdf_service_dag',
 
 t1 = BashOperator(
     task_id='parse_and_push_to_es',
-    bash_command='/usr/local/bin/python /usr/local/airflow/dags/test.py',
+    bash_command='/usr/local/bin/python /usr/local/airflow/dags/main.py pdf',
     dag=dag)
