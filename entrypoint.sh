c1ede306022cf2696c99e0619b2e1034f9069467
Luvlesh Garg <luvleshgarg@ip-10-100-10-68.ap-south-1.compute.internal>
2020-07-05 21:37:11 +0530

    ignoring health check

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,21 +22,21 @@ export TESTING_TIKA_URL="tcp://${TIKA_SERVER_ADDR}:${TIKA_SERVER_PORT}"
 export TESTING_ELASTIC_URL="tcp://${ELASTIC_SERVER_ADDR}:${ELASTIC_SERVER_PORT}"
 
 # See http://tldp.org/LDP/abs/html/devref1.html for description of this syntax.
-while ! exec 6<>/dev/tcp/${TIKA_SERVER_ADDR}/${TIKA_SERVER_PORT}; do
-    echo "$(date) - still trying to connect to TIKA at ${TESTING_TIKA_URL}"
-    sleep 5
-done
+# while ! exec 6<>/dev/tcp/${TIKA_SERVER_ADDR}/${TIKA_SERVER_PORT}; do
+#     echo "$(date) - still trying to connect to TIKA at ${TESTING_TIKA_URL}"
+#     sleep 5
+# done
 
-exec 6>&-
-exec 6<&-
+# exec 6>&-
+# exec 6<&-
 
-while ! exec 6<>/dev/tcp/${ELASTIC_SERVER_ADDR}/${ELASTIC_SERVER_PORT}; do
-    echo "$(date) - still trying to connect to Elastic search at ${TESTING_ELASTIC_URL}"
-    sleep 10
-done
+# while ! exec 6<>/dev/tcp/${ELASTIC_SERVER_ADDR}/${ELASTIC_SERVER_PORT}; do
+#     echo "$(date) - still trying to connect to Elastic search at ${TESTING_ELASTIC_URL}"
+#     sleep 10
+# done
 
-exec 6>&-
-exec 6<&-
+# exec 6>&-
+# exec 6<&-
 
 
 airflow schedular
